fc6945a52ea38a35660bf979b623fb3466bf5393
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-12-04 19:17:03 -0300

    Fazer os event listeners e a função evalExpression

--- script.js
+++ script.js
@@ -3,17 +3,66 @@ const displayBot = document.querySelector(".expression-displayer");
 
 const buttons = document.querySelectorAll(".button");
 let expression = "";
+let waitingSecondOperand = false;
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    displayBot.textContent = displayBot.textContent + event.target.textContent;
+    let input = event.target.textContent;
+    console.log(input);
+    handleInput(input);
   });
 });
 
-let waitingSecondOperand = false;
-displayBot.addEventListener("change", (event) => {
-  let lastInput = event.target.textContent.slice(
-    event.target.textContent.length,
-    -1
+function evalExpression(expression) {
+  console.log(expression);
+  let operationRegex = /[+\-*\/]/g;
+  let operation = expression.match(operationRegex).pop();
+  let leftOperand = parseInt(
+    expression.slice(0, expression.indexOf(operation))
   );
-  console.log(lastInput);
-});
+  let rightOperand = parseInt(
+    expression.slice(expression.indexOf(operation) + 1)
+  );
+  switch (operation) {
+    case "+":
+      return `${leftOperand + rightOperand}`;
+    case "-":
+      return `${leftOperand - rightOperand}`;
+    case "*":
+      return `${leftOperand * rightOperand}`;
+    case "/":
+      return `${leftOperand / rightOperand}`;
+  }
+  // return a string, the result of the expression.
+}
+function handleInput(input) {
+  switch (input) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      if (waitingSecondOperand === true) {
+        let resultOfExpression = evalExpression(
+          displayTop.textContent + displayBot.textContent
+        );
+        displayBot.textContent = "";
+        displayTop.textContent = resultOfExpression + input;
+      } else {
+        displayTop.textContent = displayBot.textContent + input;
+        displayBot.textContent = "";
+        waitingSecondOperand = true;
+      }
+
+      break;
+    case ".":
+      break;
+    case "=":
+      displayBot.textContent = evalExpression(
+        displayTop.textContent + displayBot.textContent
+      );
+      displayTop.textContent = "";
+      waitingSecondOperand = false;
+      break;
+    default:
+      displayBot.textContent += input;
+  }
+}
